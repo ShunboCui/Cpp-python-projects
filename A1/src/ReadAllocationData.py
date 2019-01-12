972af486c84752de34b6024c3a6727e495489041
Shunbo Cui <cuis13@mcmaster.ca>
2019-01-12 15:32:57 -0500

    commit message

--- A1/src/ReadAllocationData.py
+++ A1/src/ReadAllocationData.py
@@ -3,3 +3,18 @@
 #  @brief 
 #  @date 
 
+f = open('1.txt', 'r')                  
+n = 0
+list1 = []
+for line in f.readlines():
+    v = line.strip().split(':')
+    result = {}
+    result['macid'] = v[0]
+    result['fname'] = v[1]
+    result['lname'] = v[2]
+    result['gender'] = v[3]
+    result['gpa'] = v[4]
+    list1.append(result)
+f.close()
+for result in list1:
+    print(result)
