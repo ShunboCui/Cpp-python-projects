8db63bd4b70a0a1b2fdf470b4d4a2d870f6f8589
Shunbo Cui <cuis13@mcmaster.ca>
2019-01-17 16:18:52 -0500

    editing testfile

--- A1/src/testCalc.py
+++ A1/src/testCalc.py
@@ -3,3 +3,10 @@
 #  @brief 
 #  @date 
 
+import ReadAllocationData
+import CalcModule
+stdntslist = ReadAllocationData.readStdnts('1.txt')
+freelist = ReadAllocationData.readFreeChoice('3.txt')
+capacity = ReadAllocationData.readDeptCapacity('2.txt')
+allocated = CalcModule.allocate(stdntslist, freelist, capacity)
+print(allocated)
