17cd7702fa01d8535b14203f0d1088203bbf44e0
cuis13 <cuis13@mcmaster.ca>
2019-03-26 21:45:40 -0400

    updating testcases

---
+++ A3/implementation/test/testGameboard.cpp
@@ -0,0 +1,48 @@
+﻿#include "Stack.h"
+#include "catch.h"
+#include "GameBoard.h"
+#include "CardStack.h"
+#include "CardTypes.h"
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+
+using std::vector;
+
+
+TEST_CASE("General tests for GameBoard", "[GameBoard]") {
+	std::vector<CardT> d;
+	for (RankT rank = ACE; rank <= KING; rank++) {
+		for (unsigned int suit = 0; suit < 4; suit++) {
+			CardT n = { static_cast<SuitT>(suit), rank };
+			d.push_back(n);
+			d.push_back(n);
+		}
+	}
+
+	BoardT board(d);
+
+	SECTION("Test for is_valid_tab_mv") {
+		//std::cout << board.get_tab(0).top().r << std::endl;
+		//std::cout << board.get_tab(0).top().s << std::endl;
+		REQUIRE(board.is_valid_tab_mv(Foundation, 0, 0) == true);
+	}
+	
+	SECTION("Test for is_valid_waste_mv") {
+
+		REQUIRE_THROWS_AS(board.is_valid_waste_mv(Foundation, 0), std::invalid_argument);
+	}
+
+	SECTION("Second test for is_valid_waste_mv") {
+		board.deck_mv();
+		REQUIRE(board.is_valid_waste_mv(Foundation, 0) == false);
+	}
+
+	SECTION("Test for is_valid_deck_mv") {
+		REQUIRE(board.is_valid_deck_mv() == true);
+	}
+	
+
+}
---
+++ A3/implementation/test/testStack.cpp
@@ -0,0 +1,66 @@
+﻿#include "Stack.h"
+#include "catch.h"
+#include "GameBoard.h"
+#include "CardStack.h"
+#include "CardTypes.h"
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+
+using std::vector;
+
+
+
+TEST_CASE("General tests for Stack", "[StackT]") {
+	std::vector<CardT> d;
+	for (RankT rank = ACE; rank <= KING; rank++) {
+		for (unsigned int suit = 0; suit < 4; suit++) {
+			CardT n = { static_cast<SuitT>(suit), rank };
+			d.push_back(n);
+			d.push_back(n);
+		}
+	}
+
+	CardStackT stack(d);
+
+	SECTION("Tests for top") {
+
+
+		REQUIRE
+		((stack.top().s == 3
+			&& stack.top().r == 13));
+
+	}
+
+	SECTION("Tests for push") {
+		CardT newcard = { static_cast<SuitT>(1), 1 };
+		stack.push(newcard);
+		REQUIRE
+		((stack.top().s == 1
+			&& stack.top().r == 1));
+	}
+
+	SECTION("Tests for pop") {
+		CardT newcard = { static_cast<SuitT>(1), 1 };
+		stack.push(newcard);
+		stack.pop();
+		REQUIRE
+		((stack.top().s == 3
+			&& stack.top().r == 13));
+	}
+
+	SECTION("Tests for size") {
+		REQUIRE
+		((stack.size() == 104));
+	}
+
+	SECTION("Tests for toSeq") {
+		vector<CardT> seq2 = stack.toSeq();
+		CardStackT stack2(seq2);
+		REQUIRE
+		((stack2.top().s == 3
+			&& stack2.top().r == 13));
+	}
+}
