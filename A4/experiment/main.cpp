85f03ad92f7b1b95bade32b8636ad8c94146f594
cuis13 <cuis13@mcmaster.ca>
2019-04-13 17:56:55 -0400

    updating experiment

---
+++ A4/experiment/main.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <vector>
+#include "GameBoard.h"
+#include "File.h"
+
+void main() {
+	File newfile("com.txt");
+	vector<vector<int>> total = newfile.read();
+	Board board(total);
+	board.show();
+	//cout << board.get(0, 0) << endl;
+	board.next();
+	board.show();
+	//newfile.write(board.toSeq());
+
+
+}
